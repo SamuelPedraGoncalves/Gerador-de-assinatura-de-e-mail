7d5987e2400e4b578b601c0f39b0324a0e3ef32b
Samuel Pedra <139406879+SamuelPedraGoncalves@users.noreply.github.com>
2025-12-01 10:50:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function gerarAssinatura() {
             <td width="150" style="vertical-align: middle;"><span
                     class="template3__ImageContainer-sc-vj949k-0 jeScQV"
                     style="margin-right: 20px; display: block;"><img
-                        src="https://bitrix24public.com/projacseg.bitrix24.com.br/docs/pub/79b1d8ff95440f2e31bea4d7bd2c314f/showFile/?&token=sbopm2oojvow"
+                        src="https://bitrix24public.com/projacseg.bitrix24.com.br/docs/pub/222799d0982faf2d6b9f00ccc2e37471/showFile/?&token=yxu3a1rfpuso"
                         role="presentation" width="130" class="image__StyledImage-sc-hupvqm-0 gYgOut"
                         style="max-width: 130px;"></span></td>
             <td style="vertical-align: middle;">
@@ -280,6 +280,7 @@ function copiarAssinatura() {
     document.body.removeChild(textArea);
     alert('Código da assinatura copiado para a área de transferência!');
 }
+
 
 
 
